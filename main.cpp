3c9c02c57a76bad701a59a26cb8163c552249ae3
1y4nu <akanoim@gmail.com>
2025-01-07 19:43:25 -0400

    Added random door choice

--- main.cpp
+++ main.cpp
@@ -7,7 +7,21 @@ int doors[3];
 
 int main() {
 	srand(time(NULL));
-	int randomNum = rand() % 3;
-	cout << randomNum;
+	int userIn;
+	int prizeDoor = rand() % 3;
+	cout << "Pick a Door:\n\n 1 \t 2 \t 3 \n\n";
+	cout << "Type: ";
+	cin >> userIn;
+
+	if ((userIn - 1) == prizeDoor) {
+		int doorChoice = rand() % 3;
+		while (doorChoice == prizeDoor) {
+			doorChoice = rand() % 3;
+		}
+		cout << (doorChoice + 1) << " is empty.\n";
+
+	}
+	
+
 	return 0;
 }
