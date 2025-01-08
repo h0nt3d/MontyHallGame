0627493a8850bb714b8a2dd4a9fb57301cc05e52
1y4nu <akanoim@gmail.com>
2025-01-07 21:13:53 -0400

    Added the other condition incase the initial choice of the user is not the prize door

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main() {
 	srand(time(NULL));
 	int userIn;
 	int prizeDoor = rand() % 3;
+	int doorChoice = rand() % 3;
 	int remainingDoor;
 
 
@@ -13,40 +14,57 @@ int main() {
 	std::cin >> userIn;
 
 	if ((userIn - 1) == prizeDoor) {
-		int doorChoice = rand() % 3;
+
 		while (doorChoice == prizeDoor) {
 			doorChoice = rand() % 3;
 		}
 		std::cout << "\nDoor " << (doorChoice + 1) << " is empty.\n\n";
 
-		
-		//int remainingDoor;
 		for (int i = 0; i < sizeof(doors); i++) {
 			if (i != (userIn - 1) && i != doorChoice) {
 				remainingDoor = i;
 				break;
 			}
 		}
-	
-		std::cout << "Would you like to switch to Door " << (remainingDoor + 1) << " or stay on Door " << userIn << " ?\n";
-		int stayChoice;
+	}
 
-		std::cout << "1. Stay \n2. Switch \nType in number: ";
-		std::cin >> stayChoice;
+	else {
 
-		if (stayChoice == 1) {
-			if ((userIn - 1) == prizeDoor)
-				std::cout << "You won the prize!";
-			else
-				std::cout << "You lost!";
+		for (int i = 0; i < sizeof(doors); i++) {
+			if (i != (userIn - 1) && i != prizeDoor) {
+				doorChoice = i;
+				break;
+			}
 		}
-		else {
-			userIn = remainingDoor;
-			if ((userIn - 1) == prizeDoor)
-				std::cout << "You won the prize!";
-			else
-				std::cout << "You lost!";
+
+		std::cout << "\nDoor " << (doorChoice + 1) << " is empty.\n\n";
+
+		for (int i = 0; i < sizeof(doors); i++) {
+			if (i != (userIn - 1) && i != doorChoice) {
+				remainingDoor = i;
+				break;
+			}
 		}
+
+	}
+
+	std::cout << "Would you like to switch to Door " << (remainingDoor + 1) << " or stay on Door " << userIn << " ?\n";
+
+	int stayChoice;
+	std::cout << "1. Stay \n2. Switch \nType in number: ";
+	std::cin >> stayChoice;
+
+	if (stayChoice == 1) {
+		if ((userIn - 1) == prizeDoor)
+			std::cout << "You won the prize!";
+		else
+			std::cout << "You lost!";
+	}
+	else {
+		if (remainingDoor == prizeDoor)
+			std::cout << "You won the prize!";
+		else
+			std::cout << "You lost!";
 	}
 	return 0;
 }
