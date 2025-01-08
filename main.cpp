6c38bde86936d4734e00f5b68d408e01c0a3a91c
1y4nu <akanoim@gmail.com>
2025-01-07 20:47:19 -0400

    Added check for the door that has not been checked and the ability to stay or switch doors.

--- main.cpp
+++ main.cpp
@@ -1,27 +1,52 @@
 #include <iostream>
-using namespace std;
-
-int doors[3];
-
-
 
 int main() {
+	int doors[3];
+
 	srand(time(NULL));
 	int userIn;
 	int prizeDoor = rand() % 3;
-	cout << "Pick a Door:\n\n 1 \t 2 \t 3 \n\n";
-	cout << "Type: ";
-	cin >> userIn;
+	int remainingDoor;
+
+
+	std::cout << "Pick a Door:\n\n1 \t2 \t3 \n\nType: ";
+	std::cin >> userIn;
 
 	if ((userIn - 1) == prizeDoor) {
 		int doorChoice = rand() % 3;
 		while (doorChoice == prizeDoor) {
 			doorChoice = rand() % 3;
 		}
-		cout << (doorChoice + 1) << " is empty.\n";
+		std::cout << "\nDoor " << (doorChoice + 1) << " is empty.\n\n";
 
-	}
+		
+		//int remainingDoor;
+		for (int i = 0; i < sizeof(doors); i++) {
+			if (i != (userIn - 1) && i != doorChoice) {
+				remainingDoor = i;
+				break;
+			}
+		}
 	
+		std::cout << "Would you like to switch to Door " << (remainingDoor + 1) << " or stay on Door " << userIn << " ?\n";
+		int stayChoice;
 
+		std::cout << "1. Stay \n2. Switch \nType in number: ";
+		std::cin >> stayChoice;
+
+		if (stayChoice == 1) {
+			if ((userIn - 1) == prizeDoor)
+				std::cout << "You won the prize!";
+			else
+				std::cout << "You lost!";
+		}
+		else {
+			userIn = remainingDoor;
+			if ((userIn - 1) == prizeDoor)
+				std::cout << "You won the prize!";
+			else
+				std::cout << "You lost!";
+		}
+	}
 	return 0;
 }
